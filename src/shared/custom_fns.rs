a6cad91a268844276357835def31ee989def3ef9
John-Kim93 <dhkdhd123@naver.com>
2024-10-07 20:54:27 +0900

    fix : custom functions

--- src/shared/custom_fns.rs
+++ src/shared/custom_fns.rs
@@ -1,7 +1,30 @@
-fn read_num_input() -> u32 {
+/* ================= 
+    IO Functions
+================= */
+fn read_input() -> String {
     let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read input");
-    input.trim().parse::<u32>()
-        .expect("Failed to parse input as u32")
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    input
+}
+
+/* ================= 
+    Type Parsing Functions
+================= */
+fn parse_u32(string_value: &str) -> u32 {
+    string_value.trim().parse::<u32>().expect("Failed to parse input as u32")
+}
+
+fn parse_f64(string_value: &str) -> f64 {
+    string_value.trim().parse::<f64>().expect("Failed to parse input as f64")
+}
+
+/* ================= 
+    String Control Functions
+================= */
+fn split_input(string_value: &str) -> Vec<String> {
+    string_value
+        .trim()
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect()
 }
