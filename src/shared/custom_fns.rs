ceaae488c561d15655f0a5994f745b7b6518e2bf
John-Kim93 <dhkdhd123@naver.com>
2024-10-07 20:10:08 +0900

    FEAT : 커스텀 함수 관리 파일 생성

---
+++ src/shared/custom_fns.rs
@@ -0,0 +1,7 @@
+fn read_input() -> u32 {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read input");
+    input.trim().parse::<u32>()
+        .expect("Failed to parse input as u32")
+}
